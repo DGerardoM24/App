25e6dc671ef65eb05f7479936b0d5be06084c054
DGerardoM24 <101576618+DGerardoM24@users.noreply.github.com>
2022-10-31 15:07:49 -0600

    ADD HOMEPAGE

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,14 +9,73 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AppMazahua',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('AppMazahua'),
-        ),
-        body: Center(
-          child: Text('Login AppMazahua Next'),
-        ),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: _login(),
+    );
+  }
+}
+
+class _login extends StatefulWidget {
+  @override
+  __loginState createState() => __loginState();
+}
+
+class __loginState extends State<_login> {
+  TextEditingController nombre = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment(
+                  0.0, 0.99999),
+              colors: const <Color>[
+                Color.fromARGB(255, 38, 34, 255),
+                Color.fromARGB(255, 56, 232, 255),
+              ],
+              tileMode:
+                  TileMode.repeated, 
+            ),
+          ),//hola
+          child: Column(
+            children: [
+              SizedBox(//hola
+                height: 250.0,
+                width: 350.0,
+                child: Image.asset('assets/logoapp.png'),
+              )
+            ],
+          ),
       ),
     );
   }
 }
+
+/*
+decoration: const BoxDecoration(
+            gradient: const LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment(
+                  0.0, 0.99999),
+              colors: const <Color>[
+                Color.fromARGB(255, 38, 34, 255),
+                Color.fromARGB(255, 56, 232, 255),
+              ],
+              tileMode:
+                  TileMode.repeated, 
+            ),
+          ),//hola
+          child: Column(
+            children: [
+              SizedBox(//hola
+                height: 250.0,
+                width: 350.0,
+                child: Image.asset('assets/logoapp.png'),
+              )
+            ],
+          ),
+*/
